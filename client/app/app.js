ca64d018f739ee3739ba93b02eb01c48f0e1d6cd
Your Name <arssonist@yahoo.com>
2016-10-22 19:17:45 -0400

    add calender to page in app,js using medicationReminderApp.controller

--- client/app/app.js
+++ client/app/app.js
@@ -5,7 +5,8 @@ angular.module('medicationReminderApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ui.calendar'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
@@ -13,3 +14,24 @@ angular.module('medicationReminderApp', [
 
     $locationProvider.html5Mode(true);
   });
+
+
+  medicationReminderApp.controller('CalenderContoller',function($scope){
+    {
+
+   var date = new Date();
+   var d = date.getDate();
+   var m = date.getMonth();
+   var y = date.getFullYear();
+   var currentView = "month";
+
+
+   //event source that pulls from google.com
+   $scope.eventSource = {
+           url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
+           className: 'gcal-event',           // an option!
+           currentTimezone: 'America/Chicago' // an option!
+   };
+
+     };
+  })();
